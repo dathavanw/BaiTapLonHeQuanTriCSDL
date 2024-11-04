f7f63486a5678ea4377fd07f812ed75e18164485
ADMIN-PC\hi <tranquanglinh619@gmail.com>
2024-11-04 15:48:36 +0700

    no message

--- OrderForm.Designer.cs
+++ OrderForm.Designer.cs
@@ -16,11 +16,10 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.btnViewOrders = new System.Windows.Forms.Button();
             this.dgvOrders = new System.Windows.Forms.DataGridView();
-            this.btnDeleteOrder = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).BeginInit();
             this.SuspendLayout();
@@ -35,6 +34,23 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(884, 60);
             this.panel1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(152, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tên người dùng";
             // 
             // btnViewOrders
             // 
@@ -64,40 +80,11 @@
             this.dgvOrders.Size = new System.Drawing.Size(860, 429);
             this.dgvOrders.TabIndex = 1;
             // 
-            // btnDeleteOrder
-            // 
-            this.btnDeleteOrder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnDeleteOrder.Location = new System.Drawing.Point(752, 505);
-            this.btnDeleteOrder.Name = "btnDeleteOrder";
-            this.btnDeleteOrder.Size = new System.Drawing.Size(120, 35);
-            this.btnDeleteOrder.TabIndex = 2;
-            this.btnDeleteOrder.Text = "Xóa đơn hàng";
-            this.btnDeleteOrder.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(28, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(100, 16);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Tên người dùng";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(152, 21);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 6;
-          //  this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
             // OrderForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 552);
-            this.Controls.Add(this.btnDeleteOrder);
             this.Controls.Add(this.dgvOrders);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
@@ -116,7 +103,6 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnViewOrders;
         private System.Windows.Forms.DataGridView dgvOrders;
-        private System.Windows.Forms.Button btnDeleteOrder;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
     }
